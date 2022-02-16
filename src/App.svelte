0254c99177e7123f3691e8d53275e9c248e7901c
Ryan LaBouve <imryanlabouve@gmail.com>
2022-02-16 16:43:40 -0600

    Shows current URL

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,36 @@
 <script lang="ts">
-  export let name: string;
+  import { onMount } from "svelte";
+  import browser from "webextension-polyfill";
+
+  // export let var; // check main.ts if we need to pass in anything
+  let onOpenSea: boolean = false;
+  let boop: string;
+
+  async function getCurrentTab(): Promise<browser.Tabs.Tab> {
+    let queryOptions = { active: true, currentWindow: true };
+    let [tab] = await browser.tabs.query(queryOptions);
+    return tab;
+  }
+
+  onMount(async () => {
+    // get current URL
+    let tab = await getCurrentTab();
+    console.log(tab);
+    boop = tab.url || "served";
+  });
 </script>
 
 <main>
-  <h1>Hello {name}!</h1>
-  <p>
-    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
-    how to build Svelte apps.
-  </p>
+  boop: {boop}
+  {#if onOpenSea}
+    <div>Show image</div>
+    <div>Link to contract</div>
+    <div>TokenId</div>
+    <div>Token URI</div>
+    <div>Grade</div>
+  {:else}
+    Forever721 Only supports OpenSea! Read more here: Forever721.xyz
+  {/if}
 </main>
 
 <style>
@@ -16,18 +39,5 @@
     padding: 1em;
     max-width: 240px;
     margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
   }
 </style>
